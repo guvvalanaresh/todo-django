7d56a5f7ceb23b0067ea13712117c75747aa91fe
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-10-21 10:41:14 +0530

    Add Task Function

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     task = models.CharField(max_length=250)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.task
