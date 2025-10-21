45bd17fd96f0ff701688189caab7edc1f9287b28
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-10-21 11:02:00 +0530

    Mark as Done

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 # Create your views here.
@@ -7,4 +7,10 @@ from .models import Task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
     return redirect('home')
